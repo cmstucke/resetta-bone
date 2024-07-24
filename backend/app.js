da62220c17f8efb0852809eb6e6e3e6633830bb0
Taylor Musolf <tcmusolf@gmail.com>
2024-07-24 13:35:37 -0700

    add csrf route

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ require('./config/passport');
 const passport = require('passport');
 
 const usersRouter = require('./routes/api/users');
-// const csrfRouter = require('./routes/api/csrf');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -46,7 +46,7 @@ app.use(
 
 // Attach Express routers
 app.use('/api/users', usersRouter);
-// app.use('/api/csrf', csrfRouter);
+app.use('/api/csrf', csrfRouter);
 
 if (isProduction) {
     const path = require('path');
