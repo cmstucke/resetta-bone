a7a4f01d2e851c2a8ae53f6db23020ab338953bc
Tony Vongvone <tvongvone@gmail.com>
2024-07-29 16:09:42 -0700

    auth

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -2,8 +2,8 @@ module.exports = {
     secretOrKey: process.env.SECRET_KEY,
     mongoURI: process.env.MONGO_URI,
     isProduction: process.env.NODE_ENV === 'production',
-    jwtConfig: {
-      secret: process.env.JWT_SECRET,
-      expiresIn: process.env.JWT_EXPIRES_IN
-    }
+    // jwtConfig: {
+    //   secret: process.env.JWT_SECRET,
+    //   expiresIn: process.env.JWT_EXPIRES_IN
+    // }
   }
