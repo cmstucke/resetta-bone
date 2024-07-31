03ec408f34ced268146404c3a7d0c792b98104db
Tony Vongvone <tvongvone@gmail.com>
2024-07-30 18:40:35 -0700

    login page start

--- frontend/components/Splash.jsx
+++ frontend/components/Splash.jsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { useState } from 'react';
+import {useDispatch} from 'react-redux'
 
 export default function Splash() {
     return (
       <View>
-        <Text>SignUp for Resetta Bone Today!</Text>
+        <View>
+          <Pressable>
+            <Text>Log in</Text>
+          </Pressable>
+        </View>
         <StatusBar style="auto" />
       </View>
     );
