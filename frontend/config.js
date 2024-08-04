ae46d017795a302475590875f649d147de7c2937
Taylor Musolf <tcmusolf@gmail.com>
2024-08-04 15:59:12 -0700

    heroku adjustment

--- frontend/config.js
+++ frontend/config.js
@@ -1,11 +1,4 @@
 import { API_URL } from '@env';
-import Constants from "expo-constants";
-
-// console.log(Constants.expoConfig?.hostUri)
-// const uri =
-//   Constants.expoConfig?.hostUri?.split(':').shift()?.concat(':8080') ??
-//   'yourapi.com';
-// console.log(uri)
 
 export default {
   apiUrl: API_URL,
