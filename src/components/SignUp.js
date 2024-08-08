4864d43b7cfeafa7fb395a46157ef732b3ad3c6f
Tony Vongvone <tvongvone@gmail.com>
2024-08-07 22:51:19 -0700

    Signup styling

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, Pressable } from 'react-native'
+import { View, Text, TextInput, Pressable, StyleSheet, SafeAreaView } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { signup, login, clearSessionErrors } from '../../store/session';
@@ -6,7 +6,7 @@ import { signup, login, clearSessionErrors } from '../../store/session';
 export default function SignUp() {
     const [isSignUp, setIsSignUp] = useState(true);
     const [email, setEmail] = useState('');
-    const [language, setLanguage] = useState('');
+    const [language, setLanguage] = useState('en');
     const [password, setPassword] = useState('');
     const [password2, setPassword2] = useState('');
     // const [image, setImage] = useState(null);
@@ -37,8 +37,8 @@ export default function SignUp() {
         }
 
         return e =>{
-          setState(e);  
-        } 
+          setState(e);
+        }
     }
     const userSubmit = e => {
         e.preventDefault();
@@ -50,36 +50,61 @@ export default function SignUp() {
           emt: false
         };
         if(isSignUp) {
-           dispatch(signup(user));  
+           dispatch(signup(user));
         } else {
             dispatch(login(user));
         }
-        
+
     }
 
     return (
-        <View>
-            {<Text>Welcome to Resetta Bone!</Text>}
-            <TextInput 
-                style={{height: 40}} 
-                placeholder="email"
+        <SafeAreaView style={styles.signupContainer}>
+            <View style={styles.formContainer}>
+            {<Text style={{fontSize: 24, fontWeight: 'bold'}}>Welcome to Resetta Bone!</Text>}
+            <TextInput
+                style={{height: 40}}
+                placeholder="Please provide a valid email"
                 onChangeText={update('email')}
                 defaultValue={email}
             />
-            <TextInput 
-                style={{height: 40}} 
-                placeholder="password"
+            <TextInput
+                style={{height: 40}}
+                placeholder="Create a password"
                 onChangeText={update('password')}
                 defaultValue={password}
             />
-            {isSignUp && (<TextInput 
-                style={{height: 40}} 
-                placeholder="password confirmation"
+            {isSignUp && (<TextInput
+                style={{height: 40}}
+                placeholder="Confirm password"
                 onChangeText={update('password2')}
                 defaultValue={password2}
             />)}
-            <Pressable onPress={userSubmit}>{isSignUp ? <Text>SignUp</Text> : <Text>Log In</Text>}</Pressable>
+            <Pressable onPress={userSubmit}>{isSignUp ? <Text>Sign Up</Text> : <Text>Log In</Text>}</Pressable>
             <Pressable onPress={()=> setIsSignUp(!isSignUp)}>{isSignUp ? <Text>Switch to Log in</Text> : <Text>Switch to Sign Up</Text>}</Pressable>
-        </View>
+            </View>
+        </SafeAreaView>
+
     )
 }
+
+
+const styles = StyleSheet.create({
+    signupContainer : {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        backgroundColor: '#F5F5F5',
+        alignItems: 'center'
+    },
+    formContainer: {
+
+        padding: 40,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 8,
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 3
+    }
+})
