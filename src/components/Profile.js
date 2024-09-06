2e9a42eb2244a4bc9a1b3b18e8b8d0ff50151f12
Tony Vongvone <tvongvone@gmail.com>
2024-09-05 20:33:01 -0700

    more translations

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,16 +2,18 @@ import { View, Text, Pressable, SafeAreaView } from 'react-native'
 import React, { useState } from 'react';
 import { logout } from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 
 export default function Profile() {
+    const {t} = useTranslation()
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user)
     return (
         <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <View >
-                <Text>Profile</Text>
+                <Text>{t("profile")}</Text>
                 <Text>{sessionUser?.email ? sessionUser.email : "no one is logged in"}</Text>
-                <Pressable onPress={() => dispatch(logout())}><Text>Logout</Text></Pressable>
+                <Pressable onPress={() => dispatch(logout())}><Text>{t("logout")}</Text></Pressable>
             </View>
         </SafeAreaView>
     )
