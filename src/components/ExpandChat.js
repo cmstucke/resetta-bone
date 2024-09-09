0fa16a3f1ff67508e97270b80427ae0860ba498e
cmstucke <cmstucke@gmail.com>
2024-09-08 22:37:15 -0400

    expand style prop

--- src/components/ExpandChat.js
+++ src/components/ExpandChat.js
@@ -22,8 +22,8 @@ const ExpandChat = () => {
 
   return (
     <View>
-    <Pressable onPress={handleOpenChat} styles={styles.openChatPressable}>
-      <Text styles={styles.openChatText}>
+    <Pressable onPress={handleOpenChat} style={styles.openChatPressable}>
+      <Text style={styles.openChatText}>
         Assistant ^
       </Text>
     </Pressable>
@@ -31,18 +31,19 @@ const ExpandChat = () => {
   )
 }
 
-export default ExpandChat
-
 const styles = StyleSheet.create({
   openChatPressable: {
     backgroundColor: '#6495ED',
     padding: 10,
     borderRadius: 5,
     alignItems: 'center',
-    marginHorizontal: 10
+    marginHorizontal: 10,
+    color: 'blue'
   },
   openChatText: {
     color: '#fff',
     fontSize: 16,
   }
 });
+
+export default ExpandChat;
